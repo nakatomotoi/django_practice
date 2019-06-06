0b4d897aab4ba5dccf97898934112b92e8f4c46d
Tomoki Nakagawa <poilkjmnb0824@gmail.com>
2019-06-06 16:23:12 +0900

    update

---
+++ manager_project/manager/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView
+
+from manager.models import *
+
+
+class WorkerListView(TemplateView):
+    template_name = "worker_list.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+        workers = Worker.objects.all().select_related('person')  # データベースからオブジェクトを取得して
+        context['workers'] = workers  # 入れ物に入れる
+        return render(self.request, self.template_name, context)
