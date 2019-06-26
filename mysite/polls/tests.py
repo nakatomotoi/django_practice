210a22e8b525018137789a497fdbae410f92cd6d
Tomoki Nakagawa <poilkjmnb0824@gmail.com>
2019-06-26 19:34:35 +0900

    update

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,17 +9,17 @@ class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+        self.assertFalse(future_question.was_published_recently())
 
     def test_was_published_recently_with_old_question(self):
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
-        self.assrtIs(old_question.was_published_recently(), False)
+        self.assertFalse(old_question.was_published_recently())
 
     def test_was_published_recently_with_recent_question(self):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+        self.assertTrue(recent_question.was_published_recently())
 
 
 def create_question(question_text, days):
